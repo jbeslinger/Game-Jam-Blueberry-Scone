89e90cc5d0bfb6a171642e6f65cf91f9d4427c94
Josh Eslinger <jbeslinger@gmail.com>
2020-06-20 18:29:07 -0500

    Added all the art assets, set up the folders, and added all scenes to the build. Modified resolution setting.

---
+++ Blind to the Truth/Assets/Scripts/ResolutionSwapTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ResolutionSwapTest : MonoBehaviour
+{
+    private int m_ScreenWidth = 480, mScreenHeight = 270;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Screen.SetResolution(m_ScreenWidth, mScreenHeight, false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Screen.SetResolution(m_ScreenWidth * 2, mScreenHeight * 2, false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Screen.SetResolution(m_ScreenWidth * 3, mScreenHeight * 3, false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Screen.SetResolution(m_ScreenWidth * 4, mScreenHeight * 4, false);
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            Screen.SetResolution(1920, 1080, true);
+        }
+    }
+}
