28845935e6195f113ad084dcdb993383a4801a43
Josh Eslinger <jbeslinger@gmail.com>
2020-06-21 18:44:25 -0500

    Finished all of the maze cell prefabs!

---
+++ Blind to the Truth/Assets/Scripts/ExitBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+            SceneManager.LoadScene(2);
+    }
+}
