5d50f56a0322720c9ef0ac0c409d15ce32328380
Josh Eslinger <jbeslinger@gmail.com>
2020-06-20 21:34:07 -0500

    Player can now change the size of the echolocation circle.

--- Blind to the Truth/Assets/Scripts/PlayerBehavior.cs
+++ Blind to the Truth/Assets/Scripts/PlayerBehavior.cs
@@ -5,9 +5,11 @@ public class PlayerBehavior : MonoBehaviour
 {
     #region Fields
     public GameObject visibilityCirclePrefab;
+    public Transform circleVisualizer;
 
     private Rigidbody2D m_MyRigidbody2D;
-    private float m_Speed = 12f;
+    private float m_MovementSpeed = 12f;
+    private float m_MaxCircleSize = 1.5f;
     #endregion
 
     #region Methods
@@ -16,10 +18,25 @@ public class PlayerBehavior : MonoBehaviour
         m_MyRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    private float m_CircleScale = 0f;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-            SpawnEcholocationCircle();
+        if (Input.GetKey(KeyCode.Z))
+        {
+            if (m_CircleScale < m_MaxCircleSize)
+                m_CircleScale += Time.deltaTime * 3f;
+            else
+                m_CircleScale = m_MaxCircleSize;
+            
+            //TODO: Play Echolocation fillup sound on loop
+        }
+        else if (Input.GetKeyUp(KeyCode.Z))
+        {
+            SpawnEcholocationCircle(m_CircleScale);
+            m_CircleScale = 0f;
+        }
+
+        circleVisualizer.localScale = new Vector3(m_CircleScale, m_CircleScale, 1);
     }
 
     private void FixedUpdate()
@@ -32,21 +49,22 @@ public class PlayerBehavior : MonoBehaviour
         float x = 0.0f, y = 0.0f;
 
         if (Input.GetKey(KeyCode.UpArrow))
-            y = 1.0f * m_Speed;
+            y = 1.0f * m_MovementSpeed;
         else if (Input.GetKey(KeyCode.DownArrow))
-            y = -1.0f * m_Speed;
+            y = -1.0f * m_MovementSpeed;
         if (Input.GetKey(KeyCode.LeftArrow))
-            x = -1.0f * m_Speed;
+            x = -1.0f * m_MovementSpeed;
         else if (Input.GetKey(KeyCode.RightArrow))
-            x = 1.0f * m_Speed;
+            x = 1.0f * m_MovementSpeed;
 
         m_MyRigidbody2D.velocity = new Vector2(x, y);
     }
 
-    private void SpawnEcholocationCircle()
+    private void SpawnEcholocationCircle(float scale)
     {
+        //TODO: Play the echolocation noise
         GameObject echo = Instantiate(visibilityCirclePrefab, transform.position, Quaternion.identity);
-        echo.transform.localScale = new Vector3(2f, 2f, 1f);
+        echo.transform.localScale = new Vector3(scale, scale, 1f);
     }
     #endregion
 }
