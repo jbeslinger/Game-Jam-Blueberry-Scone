4e73deb06a2f686f441508aa2fa4d8e1e3473658
Josh Eslinger <jbeslinger@gmail.com>
2020-06-21 17:07:20 -0500

    The camera now snaps to maze cells as long as the player is in them!

---
+++ Blind to the Truth/Assets/Scripts/CellTriggerBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CellTriggerBehavior : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        }
+    }
+}
