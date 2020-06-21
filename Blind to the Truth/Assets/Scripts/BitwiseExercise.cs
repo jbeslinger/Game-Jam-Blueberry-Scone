0a57227feb5ea87e7598f015e3c09a0c99ca1d86
Josh Eslinger <jbeslinger@gmail.com>
2020-06-21 12:21:12 -0500

    Added prefabs for the three color keys.  Added Cell behavior to the first Cell, the Start Cell.

---
+++ Blind to the Truth/Assets/Scripts/BitwiseExercise.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class BitwiseExercise : MonoBehaviour
+{
+    [Flags]
+    public enum Exits { NONE = 0, NORTH = 1, WEST = 2, SOUTH = 4, EAST = 8 }
+
+    public Exits myExits = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            myExits = TurnBitOn(myExits, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            myExits = TurnBitOn(myExits, 2);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            myExits = TurnBitOn(myExits, 4);
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+            myExits = TurnBitOn(myExits, 8);
+    }
+
+    private Exits FlipBit(Exits value, int bitToFlip)
+    {
+        return (Exits)((int)value ^ bitToFlip);
+    }
+
+    private Exits TurnBitOn(Exits value, int bitToTurnOn)
+    {
+        return (Exits)((int)value | bitToTurnOn);
+    }
+}
