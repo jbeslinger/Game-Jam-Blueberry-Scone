48b28c703e16502b021527cf2f370698de90d2e1
Josh Eslinger <jbeslinger@gmail.com>
2020-06-20 19:40:59 -0500

    Added a Player prefab.  The player can move now.

---
+++ Blind to the Truth/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBehavior : MonoBehaviour
+{
+    #region Fields
+    private Rigidbody2D m_MyRigidbody2D;
+    private float m_Speed = 12f;
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        m_MyRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float x = 0.0f, y = 0.0f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            y = 1.0f * m_Speed;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            y = -1.0f * m_Speed;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            x = -1.0f * m_Speed;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            x = 1.0f * m_Speed;
+
+        m_MyRigidbody2D.velocity = new Vector2(x, y);
+    }
+    #endregion
+}
