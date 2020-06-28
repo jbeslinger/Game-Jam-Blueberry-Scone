44a71c0eb1f9d25fde353ae1f2d3254d2509c0fd
Josh Eslinger <jbeslinger@gmail.com>
2020-06-28 12:52:40 -0500

    Added a fake ThinkSoft logo to the beginning of the title screen to add a little more authenticity.

---
+++ Blind to the Truth/Assets/Scripts/Behaviors/PlayButtonBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonBehavior : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
