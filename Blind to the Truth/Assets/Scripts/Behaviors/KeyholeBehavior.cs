296187983072f5f3140d410a7b0a9e36276059fb
Josh Eslinger <jbeslinger@gmail.com>
2020-06-27 19:53:21 -0500

    The keyhole object now displays a message when touched.

--- Blind to the Truth/Assets/Scripts/Behaviors/KeyholeBehavior.cs
+++ Blind to the Truth/Assets/Scripts/Behaviors/KeyholeBehavior.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Animator))]
 public class KeyholeBehavior : MonoBehaviour
 {
+    #region Fields
+    public Text myTextBlurb;
+    #endregion
+
+    #region Methods
     public void DepositKeys()
     {
         GetComponent<Animator>().SetTrigger("All Keys Collected");
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+            myTextBlurb.color = Color.white;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+            myTextBlurb.color = new Color(1, 1, 1, 0);
+    }
+    #endregion
 }
---
+++ Blind to the Truth/Assets/Scripts/Behaviors/QuitTextBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class QuitTextBehavior : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
