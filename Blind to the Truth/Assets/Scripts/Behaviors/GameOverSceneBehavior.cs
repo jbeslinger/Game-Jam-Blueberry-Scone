c9e4ca9802d0f63e478fe94967149d1e76d54249
Josh Eslinger <jbeslinger@gmail.com>
2020-06-28 17:04:43 -0500

    Completed the ending cutscene.  The game exits upon completion.

---
+++ Blind to the Truth/Assets/Scripts/Behaviors/GameOverSceneBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GameOverSceneBehavior : MonoBehaviour
+{
+    public void PlayGameOverAudio()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(audio.clip);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+}
