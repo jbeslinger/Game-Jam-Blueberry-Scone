fa9371e6b93ffc873460fe439fd335eef4bfb33e
Josh Eslinger <jbeslinger@gmail.com>
2020-06-29 17:41:03 -0500

    Created game over screen animation.

--- Blind to the Truth/Assets/Scripts/Behaviors/GameOverSceneBehavior.cs
+++ Blind to the Truth/Assets/Scripts/Behaviors/GameOverSceneBehavior.cs
@@ -1,9 +1,20 @@
-﻿using UnityEditor;
+﻿using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class GameOverSceneBehavior : MonoBehaviour
 {
+    #region Constants
+    private const float MOVEMENT_UNIT = 0.5f + (1f / 8f);
+    #endregion
+
+    #region Fields
+    public Transform glitchParentObject;
+    public Sprite[] glitchSprites;
+    #endregion
+
+    #region Methods
     public void PlayGameOverAudio()
     {
         AudioSource audio = GetComponent<AudioSource>();
@@ -17,4 +28,29 @@ public class GameOverSceneBehavior : MonoBehaviour
         EditorApplication.isPlaying = false;
 #endif
     }
+
+    public void WriteGlitchTextToScreen()
+    {
+        StartCoroutine(WriteGlitchSprite());
+    }
+
+    private IEnumerator WriteGlitchSprite()
+    {
+        float waitTime = 0.001f;
+        int columns = 50, rows = 26;
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                yield return new WaitForSeconds(waitTime);
+
+                GameObject glitch = new GameObject("Glitch");
+                glitch.AddComponent<SpriteRenderer>().sprite = glitchSprites[(rows * x) + y];
+                glitch.transform.parent = glitchParentObject;
+                glitch.transform.localPosition = new Vector2(x * MOVEMENT_UNIT, -y * MOVEMENT_UNIT);
+            }
+        }
+    }
+    #endregion
 }
